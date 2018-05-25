582cba6b47c353346f238da99b941d5ebe482fde
pujie <puji@gmail.com>
2018-05-25 16:49:51 +0700

    separate header

--- index.js
+++ index.js
@@ -2,30 +2,46 @@ var express = require('express'),
     path = require('path'),
     queries = require('./modules/queries'),
     con = require('./modules/connection'),
+    bodyParser = require('body-parser'),
     app = express();
     app.engine("html",require("ejs").renderFile);
     app.set('views',path.join(__dirname,'views'));
     app.use(express.static(__dirname+'views'));
     app.use('/assets',express.static(__dirname+'/assets'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     app.get('/visits',function(req,res){
         query = queries.visits();
         con.getData(query,result=>{
             console.log("Res",result);
-            res.render("visits/index.html",{mypage:{title1:'Kunjungan',title2:'histori'},data:result});
+            res.render("visits/index.html",{
+                page:{title:'PadiApp | Kunjungan',title1:'Kunjungan',title2:'histori'},
+                data:result
+            });
         });
     })
     app.get('/offers',function(req,res){
-        query = queries.visits();
+        query = queries.offers();
         con.getData(query,result=>{
             console.log("Res",result);
-            res.render("offers/index.html",{mypage:{title1:'Penawaran',title2:'histori'},data:result});
+            res.render("offers/index.html",{
+                page:{title:'PadiApp | Penawaran',title1:'Penawaran',title2:'histori'},
+                data:result
+            });
         });
     })
-    app.get('/offersAdd',function(req,res){
+    app.get('/offersadd',function(req,res){
         query = queries.visits();
         con.getData(query,result=>{
             console.log("Rs",result);
-            res.render("offers/add.html",{mypage:{title1:'Kunjungan',title2:'histori'},data:result});
+            res.render("offers/add.html",{
+                page:{title:'PadiApp | Penambahan Penawaran',title1:'Penawaran',title2:'penambahan'},
+                data:result
+            });
         });
+    })
+    app.post('/offersave',function(req,res){
+        var data = req.body;
+        console.log(data);
     })
 app.listen(1947);
--- modules/queries.js
+++ modules/queries.js
@@ -1,6 +1,18 @@
 visits = ()=>{
     return "select * from visits";
 }
+offers = ()=>{
+    return "select * from offers"
+}
+offersave = (obj)=>{
+    sql = "insert into offers ";
+    sql+= "(kdoffer,offerdate,client_id,client_site_id,address,email,pic,phone,service,price,source,description,sale_id) ";
+    sql+= "values ";
+    sql+= "("+obj.kdoffer+","+obj.offerdate+","+obj.client_id+","+obj.client_site_id+","+obj.address+","+obj.email+","+obj.pic+","+obj.phone+","+obj.service+","+obj.price+","+obj.source+","+obj.description+","+obj.sale_id+")";
+    return sql;
+}
 module.exports = {
-    visits: visits
+    visits: visits,
+    offers: offers,
+    offersave: offersave
 }

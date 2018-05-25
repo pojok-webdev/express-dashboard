7c6b0eb9b6eb2122f68771b9c78d38fbda39b0d5
pujie <puji@gmail.com>
2018-05-25 15:46:51 +0700

    set directories and config

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ var express = require('express'),
         query = queries.visits();
         con.getData(query,result=>{
             console.log("Rs",result);
-            res.render("table_responsive.html",{mypage:{title1:'Kunjungan',title2:'histori'},data:result});
+            res.render("visits/visits.html",{mypage:{title1:'Kunjungan',title2:'histori'},data:result});
+        });
+    })
+    app.get('/offersAdd',function(req,res){
+        console.log("Sukes");
+        query = queries.visits();
+        con.getData(query,result=>{
+            console.log("Rs",result);
+            res.render("offers/add.html",{mypage:{title1:'Kunjungan',title2:'histori'},data:result});
         });
     })
 app.listen(1947);
